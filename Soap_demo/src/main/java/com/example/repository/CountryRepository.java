b2c189240432f2e6aba54aadf06b72ed7ad3b8fa
AbhishekPolamarasetty <pashek321@gmail.com>
2024-01-23 23:01:23 +0530

    output not reflected yet

---
+++ Soap_demo/src/main/java/com/example/repository/CountryRepository.java
@@ -0,0 +1,24 @@
+package com.example.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.example.entity.Country;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class CountryRepository {
+	 private static final Map<String, Country> countries = new HashMap<>();
+
+	    @PostConstruct
+	    public void initData() {
+	        // initialize countries map
+	    }
+
+	    public Country findCountry(String name) {
+	        return countries.get(name);
+	    }
+}
