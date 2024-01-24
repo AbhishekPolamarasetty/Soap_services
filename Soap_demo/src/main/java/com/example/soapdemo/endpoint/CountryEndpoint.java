36e844c5aa6c4bb4eb9c9623cc35f62952f31afd
AbhishekPolamarasetty <pashek321@gmail.com>
2024-01-24 17:57:22 +0530

    need to work on the repository

---
+++ Soap_demo/src/main/java/com/example/soapdemo/endpoint/CountryEndpoint.java
@@ -0,0 +1,33 @@
+package com.example.soapdemo.endpoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.example.soapdemo.entity.GetCountryRequest;
+import com.example.soapdemo.entity.GetCountryResponse;
+import com.example.soapdemo.repository.CountryRepository;
+
+@Endpoint
+public class CountryEndpoint {
+
+    private static final String NAMESPACE_URI = "http://www.com/example/soapdemo/entity";
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+
+        return response;
+    }
+}
