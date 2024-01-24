3e4f7d0432bc31092f33fa5f15ca9b6bffcc3b5e
AbhishekPolamarasetty <pashek321@gmail.com>
2024-01-25 00:26:35 +0530

    added the repository and worked in the soapui

--- Soap_demo/src/main/java/com/example/soapdemo/repository/CountryRepository.java
+++ Soap_demo/src/main/java/com/example/soapdemo/repository/CountryRepository.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.stereotype.Component;
 
 import com.example.soapdemo.entity.Country;
+import com.example.soapdemo.entity.Currency;
 
 import jakarta.annotation.PostConstruct;
 
@@ -17,6 +18,14 @@ public class CountryRepository {
     @PostConstruct
     public void initData() {
         // initialize countries map
+    	 Country india = new Country();
+         india.setName("India");
+         india.setCapital("Delhi");
+         india.setPopulation(123234);
+         india.setCurrency(Currency.EUR);
+
+         // Add the initialized Country object to the countries map
+         countries.put(india.getName(), india);
     }
 
     public Country findCountry(String name) {
